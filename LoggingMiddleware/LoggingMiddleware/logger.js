30af6e341d1f53f5cf169a7fda93e83dc64a323a
Mahima <22bq1a4701@vvit.net>
2025-08-01 12:00:50 +0530

    Update logger.js

--- LoggingMiddleware/LoggingMiddleware/logger.js
+++ LoggingMiddleware/LoggingMiddleware/logger.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const BASE_URL = 'http://20.244.56.144/evaluation-service/logs';
-const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJtYWhpbWFhbGx1MTZAZ21haWwuY29tIiwiZXhwIjoxNzU0MDI4NzI4LCJpYXQiOjE3NTQwMjc4MjgsImlzcyI6IkFmZm9yZCBNZWRpY2FsIFRlY2hub2xvZ2llcyBQcml2YXRlIExpbWl0ZWQiLCJqdGkiOiIyZmFkMDIxNS02NmY1LTRmZmEtOWQxYi1hOGI1NDk1ZWYxOTIiLCJsb2NhbGUiOiJlbi1JTiIsIm5hbWUiOiJtYWhpbWEgYWxsdSIsInN1YiI6ImJjNDczNWI3LWI2YTQtNDcyMy1hZTZhLWEzM2QwNzJkMDdhMiJ9LCJlbWFpbCI6Im1haGltYWFsbHUxNkBnbWFpbC5jb20iLCJuYW1lIjoibWFoaW1hIGFsbHUiLCJyb2xsTm8iOiIyMmJxMWE0NzAxIiwiYWNjZXNzQ29kZSI6IlBuVkJGViIsImNsaWVudElEIjoiYmM0NzM1YjctYjZhNC00NzIzLWFlNmEtYTMzZDA3MmQwN2EyIiwiY2xpZW50U2VjcmV0IjoiTlVKTUh3dmZTRHVUQVBGTSJ9.Tjo8B9xxXE8hxiHnlt28hBWDq8C8GuiLCiFigUz0wsQ';
+const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJtYWhpbWFhbGx1MTZAZ21haWwuY29tIiwiZXhwIjoxNzU0MDI4NzI4LCJpYXQiOjE3NTQwMjc4MjgsImlzcyI6IkFmZm9yZCBNZWRpY2NhbGllcyBQcml2YXRlIExpbWl0ZWQiLCJqdGkiOiIyZmFkMDIxNS02NmY1LTRmZmEtOWQxYi1hOGI1NDk1ZWYxOTIiLCJsb2NhbGUiOiJlbi1JTiIsIm5hbWUiOiJtYWhpbWEgYWxsdSIsInN1YiI6ImJjNDczNWI3LWI2YTQtNDcyMy1hZTZhLWEzM2QwNzJkMDdhMiJ9LCJlbWFpbCI6Im1haGltYWFsbHUxNkBnbWFpbC5jb20iLCJuYW1lIjoibWFoaW1hIGFsbHUiLCJyb2xsTm8iOiIyMmJxMWE0NzAxIiwiYWNjZXNzQ29kZSI6IlBuVkJGViIsImNsaWVudElEIjoiYmM0NzM1YjctYjZhNC00NzIzLWFlNmEtYTMzZDA3MmQwN2EyIiwiY2xpZW50U2VjcmV0IjoiTlVKTUh3dmZTRHVUQVBGTSJ9.Tjo8B9xxXE8hxiHnlt28hBWDq8C8GuiLCiFigUz0wsQ';
 
 async function log(stack, level, packageName, message) {
   try {
@@ -16,7 +16,9 @@ async function log(stack, level, packageName, message) {
         'Content-Type': 'application/json'
       }
     });
+
     console.log('Log sent:', response.data);
+    return response.data;
   } catch (error) {
     console.error('Error sending log:', error.response ? error.response.data : error.message);
   }
